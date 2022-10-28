ab711288793da0a0a5941aec0199baa7053963e6
Artur <licartur92@gmail.com>
2022-10-28 23:05:12 +0300

    Proverka2

--- src/Proverka/Proverka.java
+++ src/Proverka/Proverka.java
@@ -3,5 +3,7 @@ package Proverka;
 public class Proverka {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("its me");
     }
 }
